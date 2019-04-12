2b7f8e556cf3138bde171a283aec141d2789894c
segunfrancis <francis4dblues@gmail.com>
2019-04-12 17:19:21 +0100

    Add MessageAdapter

---
+++ app/src/main/java/com/example/computer/toku/Adapter/MessageAdapter.java
@@ -0,0 +1,52 @@
+package com.example.computer.toku.Adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.computer.toku.R;
+import com.example.computer.toku.model.User;
+
+import java.util.List;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHolder> {
+
+    public static final int MSG_TYPE_LEFT = 0;
+    public static final int MSG_TYPE_RIGHT = 1;
+
+    private Context mContext;
+    private List<User> mUser;
+
+    public MessageAdapter(Context mContext, List<User> mUser) {
+        this.mContext = mContext;
+        this.mUser = mUser;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.chat_item_left, viewGroup, false);
+        return new MessageAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUser.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
