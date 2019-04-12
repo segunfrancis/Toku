c0ae56f98549576d2378de46ad4528e6bd275ce6
segunfrancis <francis4dblues@gmail.com>
2019-04-12 14:37:55 +0100

    Add UserAdapter

---
+++ app/src/main/java/com/example/computer/toku/Adapter/UserAdapter.java
@@ -0,0 +1,63 @@
+package com.example.computer.toku.Adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.computer.toku.R;
+import com.example.computer.toku.model.User;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List<User> mUsers;
+
+    public UserAdapter(Context mContext, List<User> mUsers) {
+        this.mContext = mContext;
+        this.mUsers = mUsers;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.user_item, viewGroup, false);
+        return new UserAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        User user = mUsers.get(i);
+        viewHolder.username.setText(user.getUsername());
+        if (user.getImageURL().equals("default")) {
+            viewHolder.profileImage.setImageResource(R.mipmap.ic_launcher);
+        } else {
+            Glide.with(mContext).load(user.getImageURL()).into(viewHolder.profileImage);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUsers.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView username;
+        public ImageView profileImage;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            username = itemView.findViewById(R.id.username);
+            profileImage = itemView.findViewById(R.id.profile_image);
+        }
+    }
+}
