c49a49c1912444b609e3647abe5938eb1a8d7861
segunfrancis <francis4dblues@gmail.com>
2019-04-17 05:57:11 +0100

    Add OreoNotification - implementation

--- app/src/main/java/com/example/computer/toku/Notifications/MyFirebaseMessaging.java
+++ app/src/main/java/com/example/computer/toku/Notifications/MyFirebaseMessaging.java
@@ -1,11 +1,13 @@
 package com.example.computer.toku.Notifications;
 
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 
@@ -14,6 +16,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import static android.os.Build.VERSION_CODES.O;
 
 public class MyFirebaseMessaging extends FirebaseMessagingService {
 
@@ -26,8 +30,45 @@ public class MyFirebaseMessaging extends FirebaseMessagingService {
         FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
         if (firebaseUser != null && sent.equals(firebaseUser.getUid())) {
-            sendNotification(remoteMessage);
+
+            if (Build.VERSION.SDK_INT >= O) {
+                sendOreoNotification(remoteMessage);
+            } else
+                sendNotification(remoteMessage);
         }
+    }
+
+    private void sendOreoNotification(RemoteMessage remoteMessage) {
+        String user = remoteMessage.getData().get("users");
+        String icon = remoteMessage.getData().get("icon");
+        String title = remoteMessage.getData().get("title");
+        String body = remoteMessage.getData().get("body");
+
+        RemoteMessage.Notification notification = remoteMessage.getNotification();
+        int j = 0;
+        if (user != null) {
+            j = Integer.parseInt(user.replaceAll("[\\D]", "")); /*Request Code*/
+        }
+        Intent intent = new Intent(this, MessageActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("userId", user);
+        intent.putExtras(bundle);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, j, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        // Specifying the notification type
+        Uri defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        OreoNotification oreoNotification = new OreoNotification(this);
+        Notification.Builder builder = oreoNotification.getOreoNotification(title, body, pendingIntent,
+                defaultSound, icon);
+
+        int i = 0;
+        if (j > 0) {
+            i = j;
+        }
+
+        oreoNotification.getManager().notify(i, builder.build());
     }
 
     private void sendNotification(RemoteMessage remoteMessage) {
